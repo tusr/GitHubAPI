093dd0775bf6cae0548c5f36c4ec80530d11a71d
tusr <tmahale05@gmail.com>
2018-06-25 02:59:21 +0530

    Update MainActivity.java
    Checking the internet connectivity is added

--- app/src/main/java/com/example/tusharmahale/githubapi/MainActivity.java
+++ app/src/main/java/com/example/tusharmahale/githubapi/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.tusharmahale.githubapi;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -29,13 +31,23 @@ EditText userName;
 
                 Intent userActivity = new Intent(getApplicationContext(),UserActivity.class);
                 userActivity.putExtra("userName",userName.getText().toString());
-                if(userName.getText().toString().trim().length()<1)
+              if(!isOnline()){
+                  Toast.makeText(getApplicationContext(),"Check Internet Connection",Toast.LENGTH_SHORT).show();
+              }
+               else if(isOnline()){
+                if(userName.getText().toString().trim().length()<4)
                 {
-                    Toast.makeText(getApplicationContext(),"Enter Username:",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"Invalid Username:",Toast.LENGTH_SHORT).show();
                 }
+                else{
                 startActivity(userActivity);
-            }
-        });
+            }}
+        }});
 
     }
+boolean isOnline()
+{
+    ConnectivityManager connectivityManager =(ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+    return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnectedOrConnecting();
+}
 }
